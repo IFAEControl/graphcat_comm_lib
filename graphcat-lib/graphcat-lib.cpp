733ed668c5c448effd4b0f4fe2ffb5466ae7225b
David Roman <davidroman96@gmail.com>
2020-08-05 11:36:06 +0200

    first commit

---
+++ graphcat-lib/graphcat-lib.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include <Poco/Net/SocketAddress.h>
+#include <Poco/Net/StreamSocket.h>
+#include <Poco/Net/SocketStream.h>
+#include <Poco/StreamCopier.h>
+
+#include "graphcat-lib.hpp"
+
+using Poco::Net::IPAddress;
+
+// TODO: Ejemplo de una función de biblioteca
+void fngraphcatlib()
+{
+  //const HostEntry& entry = DNS::hostByName("www.appinf.com");
+}
+
+int test() {
+  Poco::Net::SocketAddress sa("127.0.0.1", 8080);
+  Poco::Net::StreamSocket socket(sa);
+  Poco::Net::SocketStream str(socket);
+  str << "GET / HTTP/1.1\r\n"
+         "Host: www.appinf.com\r\n"
+         "\r\n";
+
+  str.flush();
+  Poco::StreamCopier::copyStream(str, std::cout);
+	return 0;
+}
---
+++ graphcat-lib/test_client.cpp
@@ -0,0 +1,5 @@
+#include "graphcat-lib.hpp"
+
+int main() {
+    test();
+}
